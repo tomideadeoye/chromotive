5e00bde8792bf30055091c68b35b78fd20b71a21
Tomide Adeoye <tommideadeoye@gmail.com>
2023-07-25 00:58:47 +0100

    init

--- background.js
+++ background.js
@@ -8,10 +8,47 @@ const myArray = [
 	"Never leave till tomorrow that which you can do today.",
 ];
 
-chrome.windows.onCreated.addListener((window) => {
-	// Pick a random item from the array
-	const randomItem = myArray[Math.floor(Math.random() * myArray.length)];
-	// Show an alert with the selected item
-	console.alert(randomItem);
-	alert(`New window opened!\nRandom item: ${randomItem}`);
+// Sample function to return a random item from the array when chrome tab is opened
+
+function randomItem() {
+	return myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+// Sample function to create a notification when chrome tab is opened
+
+function createNotification() {
+	chrome.notifications.create({
+		type: "basic",
+		iconUrl: "icon.png",
+		title: "Hello there!",
+		message: randomItem(),
+	});
+}
+
+// My array of random content
+const randomContentArray = [
+	"Hello, world!",
+	"Welcome to my Chrome extension!",
+	"Check out my latest project on GitHub!",
+	"Have a fantastic day!",
+	"Remember to stay hydrated!",
+	"Learn something new today!",
+];
+
+// Function to select a random item from the array
+function getRandomContent() {
+	const randomIndex = Math.floor(Math.random() * randomContentArray.length);
+	return randomContentArray[randomIndex];
+}
+
+// Function to display the random content in an alert
+function showAlertWithRandomContent() {
+	const randomContent = getRandomContent();
+	alert(randomContent);
+}
+
+// Event listener for when a new Chrome tab is opened
+chrome.tabs.onCreated.addListener(function (tab) {
+	// Display the alert with random content
+	showAlertWithRandomContent();
 });
